5f92279931cbd66b4731f44483259f61e11b0892
SteveStar <71157526+SteveStar@users.noreply.github.com>
2024-11-14 19:42:11 -0500

    Updated features + added mobile

--- emailValid.js
+++ emailValid.js
@@ -1,13 +1,13 @@
-const emailLabel = document.getElementById("email-label");
+const emailLabel = document.getElementById("email-label"); // this whole group just grabs the references of the ID's
 const emailField = document.getElementById("email-field");
 const emailErr = document.getElementById("email-error");
 
 function validateEmail() {
-    if(!emailField.value.match(/\S+@\S+\.\S+/)){
-        emailErr.innerHTML = "Please enter a valid email";
+    if(!emailField.value.match(/\S+@\S+\.\S+/)){ //email field needs to follow format
+        emailErr.innerHTML = "Please enter a valid email"; //the error message in question
         return false;
     }
 
-        emailErr.innerHTML = "";
+        emailErr.innerHTML = ""; //if everything is clear, remove err msg
         return true;
 }
--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const buttons = document.querySelectorAll('.button'); // select all buttons
 
 buttons.forEach((button) => {
-    button.addEventListener('click', () => {
+    button.addEventListener('click', () => { //event listener that listens to all buttons presently.
         const text = button.previousElementSibling; 
         
         text.classList.toggle('show-details');
         
-        if (button.innerText === 'Show Details') {
-            button.innerText = 'Show Less';
+        if (button.innerText === 'Show Details') { //if its this
+            button.innerText = 'Show Less'; // change to this, which reveals a lot of stuff
         } else {
             button.innerText = 'Show Details';
         }
