8701ecf1d97fb3f317c0025d52d279608d2394c6
SteveStar <71157526+SteveStar@users.noreply.github.com>
2024-11-14 19:32:24 -0500

    Updated Code

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const buttons = document.querySelectorAll('.button'); // Select all buttons
+const buttons = document.querySelectorAll('.button'); // select all buttons
 
 buttons.forEach((button) => {
-    button.addEventListener('click', (e) => {
-        const text = button.previousElementSibling; // Get the text associated with the button
+    button.addEventListener('click', () => {
+        const text = button.previousElementSibling; 
+        
         text.classList.toggle('show-details');
+        
         if (button.innerText === 'Show Details') {
             button.innerText = 'Show Less';
         } else {
@@ -12,29 +14,3 @@ buttons.forEach((button) => {
     });
 });
 
-/*
-function extraDetails() {
-    const section = document.querySelector(".sections");
-
-    if (section.style.height === "140px" || !section.style.height) {
-        section.style.height = "300px";
-    } else {
-        section.style.height = "140px";
-    }
-}
-
-const readMoreBtn = document.querySelector('.button');
-const text = document.querySelector('.text');
-
-readMoreBtn.addEventListener('click',(e) => {
-    text.classList.toggle('show-details');
-    if(readMoreBtn.innerText === 'Show Details') {
-        readMoreBtn.innerText = 'Show Less';
-    } else {
-        readMoreBtn.innerText = 'Show Details'
-    }
-})
-
-
-
-*/
