27260a438791c02ef0f18f19a8e1cd02ccfd19e4
SteveStar <71157526+SteveStar@users.noreply.github.com>
2024-12-04 23:00:13 -0500

    Finalized Project page

--- darkmode.js
+++ darkmode.js
@@ -1,14 +1,14 @@
 let darkmode = localStorage.getItem('darkmode') //grabs darkmode
-const darkModeSwitch = document.getElementById('darkModeSwap')
+const darkModeSwitch = document.getElementById('darkModeSwap') //assigns the id that handles the button
 
 const enableDarkMode = () => {
-    document.body.classList.add('darkmode')
-    localStorage.setItem('darkmode', 'active')
+    document.body.classList.add('darkmode') //adds the darkmode class
+    localStorage.setItem('darkmode', 'active') //sets it as active
 }
 
 const disableDarkMode = () => {
-    document.body.classList.remove('darkmode')
-    localStorage.setItem('darkmode', null)
+    document.body.classList.remove('darkmode') //removes the darkmode
+    localStorage.setItem('darkmode', null) //sets it as null
 }
 
 if(darkmode === "active") enableDarkMode();
